29e673a7ec673aabbbbf9615cc91eecd59bd3fe5
mojo@pop-os <palash.swapnil@gmail.com>
2022-02-18 14:29:43 +0530

    peak element

---
+++ 162/ps.md
@@ -0,0 +1,19 @@
+# Link: <https://leetcode.com/problems/find-peak-element/>
+
+## Example
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+
+## Constraints
+
+- nums[i]!=nums[i+1]
+- O(log n)
+
+## Idea
+
+Another bs problem
+Get mid and compare with neighbours?
+How to define which side to move for peak
+
---
+++ 162/run.py
@@ -0,0 +1,27 @@
+import math
+class Solution:
+    def findPeakElement(self, nums: list[int]) -> int:
+        print(nums)
+        start , end = 0,(n:=len(nums))
+        while start<end:
+            mid = (start+end)//2
+            first = -math.inf
+            if mid-1>=0:
+                first = nums[mid-1]
+            last = -math.inf
+            if mid+1<n:
+                last = nums[mid+1]
+            print("idx:",start,end,mid)
+            middle = nums[mid]
+            print("elements:",first,middle,last)
+            if first<=middle>=last:
+                return mid
+            if first>middle:
+                end = mid
+            else:
+                start = mid+1
+        return start    
+
+#[1,2,3,1]
+nums = [int(el) for el in input()[1:-1].split(",")]
+print(Solution().findPeakElement(nums))
