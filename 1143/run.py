c596264770e7b03d7bb369b580e0d1ea54efa49b
mojo@pop-os <palash.swapnil@gmail.com>
2022-02-26 01:07:25 +0530

    optimised space

--- 1143/run.py
+++ 1143/run.py
@@ -1,16 +1,21 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text3: str) -> int:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         print(text1,text2)
         m,n = len(text1),len(text2)
-        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        if m<n: #ensuring text2 is smaller
+            m,n=n,m
+            text1,text2=text2,text1
+
+        prv = [0 for _ in range(n+1)]
+        curr = [0 for _ in range(n+1)]
 
         for i in range(1,m+1):
             for j in range(1,n+1):
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+                curr[j]=max(prv[j],curr[j-1])
                 if text1[i-1]==text2[j-1]:
-                    dp[i][j]=max(dp[i][j],1+dp[i-1][j-1])
-        print(dp)
-        return dp[m][n]
+                    curr[j]=1+prv[j-1]
+            prv,curr=curr,prv #have to prv to curr but the other way to it to just reset curr
+        return prv[n]
 
 text1, text2 = input()[1:-1], input()[1:-1]
 
