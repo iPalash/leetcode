7a1400c3818cbe9216977b53a80787de46ac4882
mojo@pop-os <palash.swapnil@gmail.com>
2022-03-09 18:11:49 +0530

    added binary solution

--- 1901/run.py
+++ 1901/run.py
@@ -5,8 +5,11 @@ class Solution:
         print(nums)
         m = len(nums)
         n = len(nums[0])
-        i,j = m//2,n//2
-        while i>=0 and i<m and j>=0 and j<n:
+        i_m, j_m = 0, m
+        i_n, j_n = 0,n
+        while 1: # No need to check since we will always find a peak, right?
+            mid_m = i = (i_m+j_m)//2
+            mid_n = j = (i_n+j_n)//2
             curr = nums[i][j]
             left = nums[i][j-1] if j>0 else -1
             right = nums[i][j+1] if j<n-1 else -1
@@ -14,13 +17,13 @@ class Solution:
             bottom = nums[i+1][j] if i<m-1 else -1
             mx = max(curr,left,right,top,bottom)
             if mx==left:
-                j=j-1
+                j_n = mid_n #j=j-1 
             elif mx==right:
-                j=j+1
+                i_n = mid_n+1 #j=j+1
             elif mx==top:
-                i=i-1
+                j_m = mid_m #i=i-1
             elif mx==bottom:
-                i=i+1
+                i_m = mid_m+1 #i=i+1
             else:
                 break
         return [i,j]
