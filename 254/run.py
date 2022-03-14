7ff3e33ba5fe7b5f7e0bff5181b2fae1f302422d
mojo@pop-os <palash.swapnil@gmail.com>
2022-03-14 20:32:15 +0530

    took some effort

--- 254/run.py
+++ 254/run.py
@@ -1,34 +1,24 @@
 import math
 class Solution:
-    def getPrimeFactors(self, n, lim, last):
-        if last>lim:
-            return
-        if n%last==0:
-            self.f.append(last)
-            self.getPrimeFactors(n//last,lim, last)
-        else:
-            self.getPrimeFactors(n,lim,last+1)
+    def getAllFactors(self,n):
+        i = 2
+        while i*i<=n:
+            if n%i==0:
+                self.f.append(i)
+            i+=1
     def rec(self, n, i, series):
-        if len(series)>=1 and series[-1]<=n:
-            self.ans.append(":".join(map(str,series+[n])))
-        
-        if i==self.fn:
+        print(n,i,series)
+        if n==0:
             return
+        
 
-        # Multiply all>= ith factor to last element of series
-        if len(series)>0: 
-            last = series[-1]
-            for j in range(i,self.fn):
-                if last*self.f[j]<= n/self.f[j]:
-                    series[-1]=last*self.f[j]
-                    self.rec(int(n/self.f[j]),j+1,series)
-                    series[-1]=last
 
         # Add all >= ith prime factor to the series
         for j in range(i,self.fn):
-            if self.f[j]<=n and (j==i or self.f[j]!=self.f[j-1]):
+            if n%self.f[j]==0 and self.f[j]<=n and (len(series)==0 or self.f[j]>=series[-1]) and self.f[j]<=n/self.f[j]:
                 series.append(self.f[j])
-                self.rec(int(n/self.f[j]),j+1,series)
+                self.ans.append(series+[int(n/self.f[j])])
+                self.rec(int(n/self.f[j]),j,series)
                 series.pop()
 
 
@@ -36,13 +26,15 @@ class Solution:
         print(n)
         self.f = []
         self.ans = []
-        self.getPrimeFactors(n, math.ceil(math.sqrt(n)), 2)
-        self.fn = len(self.f)
+        self.getAllFactors(n)
         print(self.f)
+        self.f = list(set(self.f))
+        self.fn = len(self.f)
+        print("factors",self.f)
 
         self.rec(n,0,[])
-
-        return list(map(lambda x:[int(el) for el in x.split(":")],list(set(self.ans))))
+        return self.ans
+        #return list(map(lambda x:[int(el) for el in x.split(":")],list(set(self.ans))))
 
 n = int(input())
 
