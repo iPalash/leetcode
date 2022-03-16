ba422c5762569ef036baa2440aee769911757a84
mojo@pop-os <palash.swapnil@gmail.com>
2022-03-16 20:01:28 +0530

    add logn as well

--- 1231/run.py
+++ 1231/run.py
@@ -9,7 +9,47 @@ class Solution:
         mn = 1
         mx = math.ceil(sum(sweetness)/k)
 
+        prefix = [0 for _ in range(1+n)]
+        prefix[0]=sweetness[0]
+        for i in range(n):
+            if i==0:
+                continue
+            prefix[i]=prefix[i-1]+sweetness[i]
+        print(prefix)
+
+
+        def divideFast(mid):
+            # this works by using binary search on prefix array to find parts instead of linear time
+            # Instead of o(n) it would be klogN
+
+            i=0
+            def binary(i,j,target):
+                while i<j:
+                    mid = (i+j)//2
+                    if prefix[mid]<target:
+                        i=mid+1
+                    else:
+                        j=mid
+                print("Finding",target,prefix[i])
+                return i
+            count = 0 
+            last = 0
+            print("Faster counting",mid,k)
+            print("prefix",prefix)
+            while i<n:
+                j = binary(i,n,last+mid)
+                if j>=n:
+                    return count
+                print(i,j)
+                count +=1
+                last=prefix[j]
+                i = j+1
+            return count
+
+
         def divide(mid,k):
+            return divideFast(mid)
+            print("Fast count:",mid,divideFast(mid))
             curr = 0
             count = 0
             parts = []
